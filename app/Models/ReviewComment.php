a10638450d4df5580ce8314f98e2da44cae3362d
marek311 <mareklacko311@icloud.com>
2023-12-28 17:43:59 +0100

    added review comment functionality

---
+++ app/Models/ReviewComment.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReviewComment extends Model
+{
+    use HasFactory;
+    protected $table = 'review_comment';
+    protected $fillable = ['content'];
+
+    public function review()
+    {
+        return $this->belongsTo(Review::class);
+    }
+}
