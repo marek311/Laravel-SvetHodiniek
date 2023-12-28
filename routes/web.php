da0c0c92f1c387d6d60cf230e6cdca8710d05930
marek311 <mareklacko311@icloud.com>
2023-12-28 22:08:57 +0100

    added login and register functionality

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\GalleryPostController;
