ca83247b031ebd0b1eed70f872a861588052d105
marek311 <mareklacko311@icloud.com>
2023-12-30 15:19:18 +0100

    profile view styling update

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,6 +6,7 @@
     <title>Profile</title>
     <link rel="stylesheet" href="{{ asset('styling_general.css') }}">
     <link rel="stylesheet" href="{{ asset('styling_profile.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
 </head>
 <body>
 <div class="profile-container">
@@ -14,8 +15,10 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
         </svg>
     </a>
-    <div class="container-h-center">
-        <h1>Profile</h1>
+    <div class="ms-auto">
+        <a href="profile" class="nav-link">
+            <i class="bi bi-person person-icon"></i>
+        </a>
     </div>
     <div class="buttons-container">
         <a href="register" class="button">Register</a>
