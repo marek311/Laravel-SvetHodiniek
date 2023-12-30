680b09a10e8c3271be63ba67ffd6843a009cefdf
marek311 <mareklacko311@icloud.com>
2023-12-30 15:02:58 +0100

    profile view styling update

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,22 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AboutUs</title>
+    <title>Profile</title>
     <link rel="stylesheet" href="{{ asset('styling_general.css') }}">
     <link rel="stylesheet" href="{{ asset('styling_profile.css') }}">
 </head>
 <body>
-<a href="{{ route('home') }}" class="back-link">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="gold" class="back-arrow" width="5%">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
-    </svg>
-</a>
-<div class="container-h-center">
-    <h1>Profile</h1>
-</div>
-<div class="buttons-container">
-    <a href="register" class="button">Register</a>
-    <a href="login" class="button">Login</a>
+<div class="profile-container">
+    <a href="{{ route('home') }}" class="back-link">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="gold" class="back-arrow" width="5%">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
+        </svg>
+    </a>
+    <div class="container-h-center">
+        <h1>Profile</h1>
+    </div>
+    <div class="buttons-container">
+        <a href="register" class="button">Register</a>
+        <a href="login" class="button">Login</a>
+    </div>
 </div>
 </body>
 </html>
