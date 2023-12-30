1b7d8d2d963aadd093c966009b047f4d141ab34e
marek311 <mareklacko311@icloud.com>
2023-12-30 15:23:48 +0100

    profile view styling update

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -11,7 +11,7 @@
 <body>
 <div class="profile-container">
     <a href="{{ route('home') }}" class="back-link">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="gold" class="back-arrow" width="5%">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="gold" class="back-arrow" width="15%">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
         </svg>
     </a>
