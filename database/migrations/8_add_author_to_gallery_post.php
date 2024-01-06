82197950231dabacae64ee59258adbd8da7867fb
marek311 <mareklacko311@icloud.com>
2024-01-06 18:43:07 +0100

    author of gallery post added

--- database/migrations/8_add_author_to_gallery_post.php
+++ database/migrations/8_add_author_to_gallery_post.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('gallery_post', function (Blueprint $table) {
-            //
+            $table->foreignId('user_id')->constrained('users');
         });
     }
 
@@ -22,7 +22,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('gallery_post', function (Blueprint $table) {
-            //
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
         });
     }
 };
