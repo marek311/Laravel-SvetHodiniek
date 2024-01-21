6cfecac92d5867ab9aa6aba04a2c9a4dab46023c
marek311 <mareklacko311@icloud.com>
2024-01-21 14:49:31 +0100

    code cleanup

--- public/scriptHandleFileSelect.js
+++ public/scriptHandleFileSelect.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const pictureFileInput = document.getElementById('pictureFile');
     const fileNamePlaceholder = document.getElementById('file-name-placeholder');
-
     if (pictureFileInput) pictureFileInput.addEventListener('change', handleFileSelect);
-
     function handleFileSelect(event) {
         const selectedFile = event.target.files[0];
         if (selectedFile) fileNamePlaceholder.textContent = selectedFile.name;
