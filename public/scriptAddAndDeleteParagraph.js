ab56cbea7b85f502b8d3157499bd769cc9aff8e5
marek311 <mareklacko311@icloud.com>
2024-01-21 09:53:16 +0100

    script for adding and removing text inputs for creating review functionality

---
+++ public/scriptAddAndDeleteParagraph.js
@@ -0,0 +1,27 @@
+function addParagraph() {
+    const container = document.getElementById('paragraphsContainer');
+    const newContainer = document.createElement('div');
+    newContainer.classList.add('paragraphInputContainer');
+    const newInput = document.createElement('input');
+    newInput.type = 'text';
+    newInput.classList.add('paragraphInput');
+    newInput.name = 'content[]';
+    newInput.required = true;
+    const deleteButton = document.createElement('button');
+    deleteButton.type = 'button';
+    deleteButton.textContent = 'Delete Paragraph';
+    deleteButton.onclick = function () {
+        deleteParagraph(this);
+    };
+    newContainer.appendChild(document.createElement('br'));
+    newContainer.appendChild(document.createTextNode('Paragraph: '));
+    newContainer.appendChild(newInput);
+    newContainer.appendChild(deleteButton);
+    container.appendChild(newContainer);
+}
+
+function deleteParagraph(button) {
+    const container = button.parentNode;
+    const paragraphsContainer = document.getElementById('paragraphsContainer');
+    paragraphsContainer.removeChild(container);
+}
