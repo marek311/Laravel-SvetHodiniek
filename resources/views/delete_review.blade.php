4bbde3c2b1b9a839ec0fd3ddc6c18640537284ca
marek311 <mareklacko311@icloud.com>
2024-01-21 11:28:19 +0100

    names and titles update

--- resources/views/delete_review.blade.php
+++ resources/views/delete_review.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Delete Review</title>
+    <title>Vymazanie recenzie</title>
     <link rel="stylesheet" href="{{ asset('styling_editPage.css') }}">
 </head>
 <body>
@@ -13,16 +13,16 @@
     </svg>
 </a>
 <div class="container-h-center">
-    <h1>Delete Review</h1>
+    <h1>Vymazanie recenzie</h1>
 </div>
 <div class="form-container">
     <form action="{{ route('review.delete', ['watch_name' => $review->watch_name]) }}" method="post">
         @csrf
         @method('DELETE')
-        <label for="submit_button">Are you sure you want to delete this review?</label>
+        <label for="submit_button">Naozaj chceš vymazať túto recenziu?</label>
         <div class="button-container">
-            <button id="submit_button" type="submit">Delete Review</button>
-            <a href="{{ route('reviews') }}" class="cancel-button">Cancel</a>
+            <button id="submit_button" type="submit">Vymazať</button>
+            <a href="{{ route('reviews') }}" class="cancel-button">Zrušiť</a>
         </div>
     </form>
 </div>
