96d15acafb96a7a73e6385fe9c9076ec706454a7
marek311 <mareklacko311@icloud.com>
2024-01-21 11:11:14 +0100

    names and titles update

--- resources/views/delete_galleryPost.blade.php
+++ resources/views/delete_galleryPost.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Delete Gallery Post</title>
+    <title>Vymazanie postu</title>
     <link rel="stylesheet" href="{{ asset('styling_editPage.css') }}">
 </head>
 <body>
@@ -13,16 +13,16 @@
     </svg>
 </a>
 <div class="container-h-center">
-    <h1>Delete Gallery Post</h1>
+    <h1>Vymazanie postu</h1>
 </div>
 <div class="form-container">
     <form action="{{ route('gallery.delete', ['id' => $galleryPost->id]) }}" method="post">
         @csrf
         @method('DELETE')
-        <label for="submit_button">Are you sure you want to delete this gallery post?</label>
+        <label for="submit_button">Naozaj chceš vymazať tento post?</label>
         <div class="button-container">
-            <button id="submit_button" type="submit">Delete Gallery Post</button>
-            <a href="{{ route('gallery') }}" class="cancel-button">Cancel</a>
+            <button id="submit_button" type="submit">Vymazať</button>
+            <a href="{{ route('gallery') }}" class="cancel-button">Zrušiť</a>
         </div>
     </form>
 </div>
