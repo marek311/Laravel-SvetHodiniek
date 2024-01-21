4f09b2c13c36d758f0087c3fa2ba07791d0a1460
marek311 <mareklacko311@icloud.com>
2024-01-21 12:48:49 +0100

    names and titles update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,15 +5,12 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header" style="background-color: darkgreen; color: gold;">{{ __('Login') }}</div>
-
+                    <div class="card-header" style="background-color: darkgreen; color: gold;">{{ __('Prihlásenie') }}</div>
                     <div class="card-body" style="background-color: gold; color: darkgreen;">
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
-
                             <div class="row mb-3">
-                                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
+                                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Emailová adresa') }}</label>
                                 <div class="col-md-6">
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
@@ -24,13 +21,10 @@
                                     @enderror
                                 </div>
                             </div>
-
                             <div class="row mb-3">
-                                <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
+                                <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Heslo') }}</label>
                                 <div class="col-md-6">
                                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
                                     @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -38,30 +32,11 @@
                                     @enderror
                                 </div>
                             </div>
-
-                            <div class="row mb-3">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
                             <div class="row mb-0">
                                 <div class="col-md-8 offset-md-4">
                                     <button type="submit" class="btn btn-primary" style="background-color: darkgreen; color: gold;">
-                                        {{ __('Login') }}
+                                        {{ __('Prihlásiť sa') }}
                                     </button>
-
-                                    @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}" style="color: darkgreen;">
-                                            {{ __('Forgot Your Password?') }}
-                                        </a>
-                                    @endif
                                 </div>
                             </div>
                         </form>
