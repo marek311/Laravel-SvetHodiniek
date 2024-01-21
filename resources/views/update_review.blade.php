21bcbf8734428043d015720443e4a97d5e107134
marek311 <mareklacko311@icloud.com>
2024-01-21 18:14:55 +0100

    client checks added

--- resources/views/update_review.blade.php
+++ resources/views/update_review.blade.php
@@ -20,12 +20,12 @@
     @csrf
     @method('PUT')
     <label for="title">Meno hodiniek:</label>
-    <input type="text" id="title" name="title" value="{{ $review->watch_name }}" required>
+    <input type="text" id="title" name="title" value="{{ $review->watch_name }}" pattern="[A-Za-z0-9\s]+" title="Prosím, zadajte platné meno, iba písmená, číslice a medzery" required>
     <div id="paragraphsContainer">
         @foreach ($review->paragraphs as $index => $paragraph)
             <div class="paragraphInputContainer">
                 <label for="content{{ $index }}">Obsah recenzie:</label>
-                <textarea id="content{{ $index }}" name="content[{{ $index }}]">{{ $paragraph->paragraph_text }}</textarea>
+                <textarea id="content{{ $index }}" name="content[{{ $index }}]" pattern="[A-Za-z0-9\s.,!?]+" title="Prosím, zadajte platný obsah, iba písmená, číslice, medzery a .,!?" required>{{ $paragraph->paragraph_text }}</textarea>
                 <button type="button" onclick="deleteParagraph(this)">Vymaž paragraf</button>
             </div>
         @endforeach
